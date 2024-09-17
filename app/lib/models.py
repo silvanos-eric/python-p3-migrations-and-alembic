8c98de7edb0d70d70508f5e865dbd686919ebb90
Silvanos Eric <ericoderito@gmail.com>
2024-09-17 15:41:49 +0300

    feat(models): add Student model and migration
    - Added Student model to models.py with the following fields: id, name, email, grade, birthday,
      enrolled_date
    - Included constraints: grade between 1 and 12, unique email
    - Created migration script to add students table with the appropriate schema and constraints
    - Updated migration_test.db accordingly

--- app/lib/models.py
+++ app/lib/models.py
@@ -1,8 +1,25 @@
-#!/usr/bin/env python3
+from datetime import datetime
 
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine, desc, CheckConstraint, UniqueConstraint,
+                        Column, DateTime, Integer, String)
 from sqlalchemy.orm import declarative_base
 
 engine = create_engine('sqlite:///migration_test.db')
 
 Base = declarative_base()
+
+
+class Student(Base):
+    __tablename__ = 'students'
+    __table_args__ = (CheckConstraint('grade BETWEEN 1 AND 12',
+                                      name='grade_between_1_and_12'), )
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), index=True)
+    email = Column(String(55), unique=True)
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return f"Student({self.id}, {self.name}, {self.grade})"
